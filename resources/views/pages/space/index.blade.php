4ba4f483dad1a5bc263e777868e5cb8190b66c50
Jeryhardianto <hardiantojr29@gmail.com>
2020-04-27 19:03:00 +0700

    first commit

---
+++ resources/views/pages/space/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <x-space></x-space>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+             @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+            @foreach ($spaces as $space)           
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        {{ $space->title }}
+                        <form action="#">
+                            @csrf @method('DELETE')
+                            <button type="submit" class="btn btn-sm btn-danger float-lg-right">Delete</button>
+                            <a href="#" class="btn btn-sm btn-info float-lg-right text-white">Edit</a>
+                        </form>
+                    </h5>
+                    <h5 class="card-subtitle">{{ $space->address }}</h5>
+                    <h5 class="card-text">{{ $space->description }}</h5>
+                    <a href="#" class="card-link">Direction</a>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+     <div class="row justify-content-center">
+            {{  $spaces->links() }}
+        </div>
+</div>
+@endsection
